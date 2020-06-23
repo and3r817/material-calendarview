1a3140bde475011adcc76afa033b89653d6944d0
khaled_aboshama <khaled.aboshama@pharos-solutions.de>
2020-06-23 17:38:02 +0200

    Formating the file

--- library/src/main/java/com/prolificinteractive/materialcalendarview/CalendarPager.java
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/CalendarPager.java
@@ -1,13 +1,12 @@
 package com.prolificinteractive.materialcalendarview;
 
 import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.viewpager.widget.ViewPager;
-
-import android.util.AttributeSet;
-import android.view.MotionEvent;
 
 /**
  * Custom ViewPager that allows swiping to be disabled.
